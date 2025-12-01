82369846ee422ba197712ec3a3ed80fa72671397
Daniel Andreas Wang <danielaw@getacademy.no>
2025-12-01 15:47:11 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+import CryptoForm from './components/CryptoForm.vue';
+
+const submittedText = ref('');
+
+function handleTextSubmitted(text) {
+  submittedText.value = text;
+}
+</script>
+
+<template>
+  <CryptoForm @text-submitted="handleTextSubmitted" />
+  <div v-if="submittedText">
+    <h2>Submitted Text:</h2>
+    <pre>{{ submittedText }}</pre>
+  </div>
+</template>
---
+++ src/components/CryptoForm.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+
+const inputText = ref('');
+const emit = defineEmits(['text-submitted']);
+
+function handleSubmit() {
+    emit('text-submitted', inputText.value);
+}
+</script>
+
+<template>
+    <form @submit.prevent="handleSubmit">
+        <textarea v-model="inputText" id="crypto-input" rows="10" cols="50" placeholder="Type your plaintext or ciphertext here..."></textarea>
+        <br />
+        <button type="submit">Submit</button>
+    </form>
+</template>
